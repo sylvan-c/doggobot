31752c45ce7a23b9aae1dea6563666092d672db7
sylvan-c <67205854+sylvan-c@users.noreply.github.com>
2020-11-26 14:31:29 +0000

    Update doggobot.sh

--- doggobot.sh
+++ doggobot.sh
@@ -2,7 +2,7 @@
 
 while true; do
     if [ `date +%H` == "08" ]; then
-        python3 /home/$USER/dogs/doggobot.py;
+        python3 /home/$USER/doggobot/doggobot.py $USER;
         dogs=/home/$USER/doggobot/*.jpg;
         neomutt -a $dogs -s "Fresh Doggo Delivery" -- user@domain.com < /home/$USER/doggobot/email.txt && rm $dogs;
         sleep 6000;
