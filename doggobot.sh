e571810bd29230d65a39942793b767ca3a8e85fe
sylvan-c <67205854+sylvan-c@users.noreply.github.com>
2020-11-26 14:22:25 +0000

    Change directory name to doggobot

--- doggobot.sh
+++ doggobot.sh
@@ -3,8 +3,8 @@
 while true; do
     if [ `date +%H` == "08" ]; then
         python3 /home/$USER/dogs/doggobot.py;
-        dogs=/home/$USER/dogs/*.jpg;
-        neomutt -a $dogs -s "Fresh Doggo Delivery" -- user@domain.com < /home/$USER/dogs/email.txt && rm $dogs;
+        dogs=/home/$USER/doggobot/*.jpg;
+        neomutt -a $dogs -s "Fresh Doggo Delivery" -- user@domain.com < /home/$USER/doggobot/email.txt && rm $dogs;
         sleep 6000;
     fi
     sleep 1200;
