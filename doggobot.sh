8143d2afccc296ef57cfff03a50224388732086f
sylvan-c <67205854+sylvan-c@users.noreply.github.com>
2020-11-29 18:20:05 +0000

    Read target email as arg

--- doggobot.sh
+++ doggobot.sh
@@ -4,7 +4,7 @@ while true; do
     if [ `date +%H` == "08" ]; then
         python3 /home/$USER/doggobot/doggobot.py $USER;
         dogs=/home/$USER/doggobot/*.jpg;
-        neomutt -a $dogs -s "Fresh Doggo Delivery" -- user@domain.com < /home/$USER/doggobot/email.txt && rm $dogs;
+        neomutt -a $dogs -s "Fresh Doggo Delivery" -- $1 < /home/$USER/doggobot/email.txt && rm $dogs;
         sleep 6000;
     fi
     sleep 1200;
