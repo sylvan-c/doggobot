f236778d0999f8a207878faf2b54f505ff431b51
sylvan-c <67205854+sylvan-c@users.noreply.github.com>
2020-12-01 12:00:37 +0000

    Pull images from pics directory

--- doggobot.sh
+++ doggobot.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ ! -d /home/$USER/doggobot/pics ]; then
+    mkdir /home/$USER/doggobot/pics;
+fi
+
 while true; do
     if [ `date +%H` == "08" ]; then
         python3 /home/$USER/doggobot/doggobot.py $USER;
-        dogs=/home/$USER/doggobot/*.jpg;
+        dogs=/home/$USER/doggobot/pics/*;
         neomutt -a $dogs -s "Fresh Doggo Delivery" -- $1 < /home/$USER/doggobot/email.txt && rm $dogs;
         sleep 80000;
     fi
